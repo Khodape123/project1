1f7ed5b88cfe77b15adcb25532e66714967d3997
Khodape123 <khodapep123@gmail.com>
2024-03-07 22:44:29 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,103 @@
+import './Layout.css';
+function Home(){
+
+    return(
+
+        <>
+        <div className='home'> 
+         <h1> Title </h1>
+         <p>This is paragraph</p>
+        <button> Read More</button>
+        </div>
+        <div className='home1'> 
+        <h1 className='new'> Classes</h1>
+        <p className='new1'> this is paragragh</p>
+    </div>
+
+
+        <div className='row'>
+        <div className='coloumn left'>  
+        <img src='flower1.jpeg'alt='img'/> 
+         <h3> Basic Technical Writing</h3>
+         <p> This course focuses on understading the basic of thechnical writing and devloping technical writing skills</p>
+         <button> View</button>
+         </div>
+         
+         
+         <div className='coloumn middle'>  
+        <img src='flower1.jpeg'alt='img'/> 
+         <h3> Basic Technical Writing</h3>
+         <p> This course focuses on understading the basic of thechnical writing and devloping technical writing skills</p>
+         <button> View</button>
+         </div>
+
+         <div className='coloumn right'>  
+        <img src='flower1.jpeg'alt='img'/> 
+         <h3> Basic Technical Writing</h3>
+         <p> This course focuses on understading the basic of thechnical writing and devloping technical writing skills</p>
+         <button> View</button>
+         </div>
+          
+         </div>
+
+
+         <div className='row'>
+        <div className='coloumn left'>  
+        <img src='flower1.jpeg'alt='img'/> 
+         <h3> Basic Technical Writing</h3>
+         <p> This course focuses on understading the basic of thechnical writing and devloping technical writing skills</p>
+         <button> View</button>
+         </div>
+         
+         
+         <div className='coloumn middle'>  
+        <img src='flower1.jpeg'alt='img'/> 
+         <h3> Basic Technical Writing</h3>
+         <p> This course focuses on understading the basic of thechnical writing and devloping technical writing skills</p>
+         <button> View</button>
+         </div>
+
+         <div className='coloumn right'>  
+        <img src='flower1.jpeg'alt='img'/> 
+         <h3> Basic Technical Writing</h3>
+         <p> This course focuses on understading the basic of thechnical writing and devloping technical writing skills</p>
+         <button> View</button>
+         </div>
+          
+         </div>
+         <div className='row'>
+        <div className='coloumn left'>  
+        <img src='flower1.jpeg'alt='img'/> 
+         <h3> Basic Technical Writing</h3>
+         <p> This course focuses on understading the basic of thechnical writing and devloping technical writing skills</p>
+         <button> View</button>
+         </div>
+         
+         
+         <div className='coloumn middle'>  
+        <img src='flower1.jpeg'alt='img'/> 
+         <h3> Basic Technical Writing</h3>
+         <p> This course focuses on understading the basic of thechnical writing and devloping technical writing skills</p>
+         <button> View</button>
+         </div>
+
+         <div className='coloumn right'>  
+        <img src='flower1.jpeg'alt='img'/> 
+         <h3> Basic Technical Writing</h3>
+         <p> This course focuses on understading the basic of thechnical writing and devloping technical writing skills</p>
+         <button> View</button>
+         </div>
+          
+         </div>
+         
+         
+      
+    
+         
+         
+        
+        
+        </>
+    )
+}
+export default Home;
